d74ddf2cb712226d515ab800cb00f055f65aef2f
AllenGX <1413907153@qq.com>
2019-12-06 14:29:37 +0800

    a

--- myhooktools/commitmsg.py
+++ myhooktools/commitmsg.py
@@ -1,17 +1,19 @@
 # -*- coding:utf-8 -*-
 import sys
-
-class CommitMsgException(Exception):
-    pass
+import myhooktools.astcheck as ast_check
 
 class GitCommitMsg:
 
     def __init__(self, args):
+        self.access = 1
         self._msg_info = args[1]
 
-    def Print(self):
-        print(self._msg_info)
+    def CheckMsg(self):
+        if len(self._msg_info)<5:
+            self.access = 0
+            print("\033[0;31;40m \t%s len < 5 \033[0m" % self._msg_info)
 
 if __name__ == '__main__':
     hook_obj = GitCommitMsg(sys.argv)
-    hook_obj.Print()
+    access = hook_obj.CheckMsg()
+    sys.exit(access)
