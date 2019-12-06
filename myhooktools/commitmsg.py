daef339947df22bab740cfa69622fcae28790806
AllenGX <1413907153@qq.com>
2019-12-06 14:48:43 +0800

    a

--- myhooktools/commitmsg.py
+++ myhooktools/commitmsg.py
@@ -1,21 +1,33 @@
 # -*- coding:utf-8 -*-
 import sys
-argv = sys.argv
-sys.path.append(argv[1])
+path = sys.argv[1]
+sys.path.append(path)
+
+msg_path = path + "/" + ".git/COMMIT_EDITMSG"
 
 class GitCommitMsg:
 
-    def __init__(self, args):
+    def __init__(self):
         self.access = 1
-        self._msg_info = args[2]
-        print(self._msg_info)
 
     def CheckMsg(self):
-        if len(self._msg_info)<5:
+        try:
+            with open(msg_path, "r", encoding='UTF-8') as fp:
+                _msg_info = fp.read()
+                if len(self._msg_info)<5:
+                    self.access = 0
+                    print("\033[0;31;40m \t%s len < 5 \033[0m" % self._msg_info)
+        except FileNotFoundError as e:
+            print("\033[0;31;33m \t%s not find \033[0m" % ".git/COMMIT_EDITMSG")
             self.access = 0
-            print("\033[0;31;40m \t%s len < 5 \033[0m" % self._msg_info)
+        except Exception as e:
+            self.access = 0
+            raise e
+        else:
+            print("")
+
 
 if __name__ == '__main__':
-    hook_obj = GitCommitMsg(sys.argv)
+    hook_obj = GitCommitMsg()
     access = hook_obj.CheckMsg()
     sys.exit(access)
--- myhooktools/main.py
+++ myhooktools/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import sys
+
 argv = sys.argv
 
 sys.path.append(argv[1])
-import myhooktools.precommithook as commit
 
+import myhooktools.precommithook as commit
 
 if __name__ == '__main__':
     hook_obj = commit.GitPreHook(sys.argv)
